7f752809abf4594572c650f75e2a08a130b38ea6
CardsClaw <joshr1979@gmail.com>
2023-07-17 17:42:59 -0500

    index.js initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require('inquirer');
+const Department = require('./db/models/department');
+const Role = require('./db/models/role');
+const Employee = require('./db/models/employee');
+const sequelize = require('./db/connection');
+
