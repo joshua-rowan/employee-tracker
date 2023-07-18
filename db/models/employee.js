d0ec77c28af60d24cfab6e2747b53661c276564c
CardsClaw <joshr1979@gmail.com>
2023-07-17 22:47:58 -0500

    added sequelize features

--- db/models/employee.js
+++ db/models/employee.js
@@ -1,44 +1,47 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../connection');
+const sequelize = require('../config/connection');
 
 class Employee extends Model {}
 
 Employee.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        first_name: {
-            type: DataTypes.STRING(30),
-            allowNull: false,
-        },
-        last_name: {
-            type: DataTypes.STRING(30),
-            allowNull: false,
-        },
-        role_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: 'role',
-                key: 'id',
-            },
-        },
-        manager_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'employee',
-                key: 'id',
-            },
-        },
-    }, 
-    {
-        sequelize,
-        timestamps: false,
-        modelName: 'role',
-    }
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'role',
+        key: 'id',
+      },
+    },
+    manager_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'employee',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'employee',
+  }
 );
 
 module.exports = Employee;
